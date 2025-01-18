b3cd565ac9bc92cc665d1a3c162ba0ada80ea3aa
Sayan Dafadar <dafadarts@gmail.com>
2025-01-18 10:25:57 +0530

    tried

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ export default ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL: "multik8s.site",
+      baseURL: "http://multik8s.site",
       headers: req.headers,
     });
   } else {
