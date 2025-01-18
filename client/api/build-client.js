2b8573dc105b355bb08c0ca128aff1e2326b47b3
Sayan Dafadar <dafadarts@gmail.com>
2025-01-18 09:44:55 +0530

    build-client

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,8 @@ const buildClient = ({ req }) => {
   if (typeof window === "undefined") {
     // We are on the server!
     return axios.create({
-      baseURL: "https://multik8s.site",
+      baseURL:
+        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
       headers: req.headers,
     });
   } else {
